5e40e15b666257f7265e6f1e53d87b7f24d03482
Evgeny Danilenko <evgeny_d@outlook.com>
2020-12-20 16:53:37 +0300

    Minor changes

--- ConsoleArrayAverage/Program.cs
+++ ConsoleArrayAverage/Program.cs
@@ -12,19 +12,19 @@ namespace ConsoleArrayAverage
                 newArray[i] = array[i];
             array = newArray;
         }
-        
-        static void Main(string[] args)
+
+        static void AverageMethod ()
         {
             Int64[] myArray; //Deaclare an array
             int t = 1;
-            
+
             myArray = new Int64[t]; //Initialize an array
 
-            for (int i = 0; i < t; i++ )
+            for (int i = 0; i < t; i++)
             {
                 Console.WriteLine("Enter int64 value for the my Array: ");
-                myArray[i] = Convert.ToInt64(Console.ReadLine()); //Assigning value to an array element          
-                
+                myArray[i] = Convert.ToInt64(Console.ReadLine()); //Assigning value to an array element 
+
                 Console.WriteLine("Array length = " + myArray.Length);
                 Console.WriteLine("Array average = " + myArray.Average());
                 t++;
@@ -35,7 +35,47 @@ namespace ConsoleArrayAverage
                 Console.Write("\n");
                 Resize(ref myArray, t); //Change array length
             }
+        }
 
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.WriteLine("Enter int64 value for myArray[] OR Enter x to stop.");
+            
+            string varToEnter = Console.ReadLine();
+
+            if (varToEnter == "x" || varToEnter == "X")
+            {
+                Console.Clear();
+                Console.Write("Console cleared");               
+            }
+            else
+            {
+                try
+                {
+                    Convert.ToInt64(varToEnter); //Assigning value to an array element                                                                                               
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+
+            /*
+                finally
+            {
+                if varForArray == "x";
+            }
+            */
+            
+
+
+
+
+
+
+            //AverageMethod();
         }
     }
 }
